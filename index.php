4e3484d3c5454c7225dd4fd087d261d4fcbf02b5
Pakpoom@SUT <wisedsri2@gmail.com>
2018-06-09 11:21:26 +0700

    TACE Web Starter
    สร้าง Html Theme

---
+++ index.php
@@ -0,0 +1,240 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Font-awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+
+    <!-- Bootstrap CSS -->
+    <!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">-->
+    <link rel="stylesheet" href="main.css" type="text/css"> </head>
+
+    <title> . : TACE : สมาคมสหกิจศึกษาไทย </title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-md bg-primary navbar-dark">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <i class="fa d-inline fa-lg fa-cloud"></i>
+            <b>TACE</b>
+        </a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse text-center justify-content-end" id="navbar2SupportedContent">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="fa d-inline fa-lg fa-bookmark-o"></i> Bookmarks</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <i class="fa d-inline fa-lg fa-envelope-o"></i> Contacts</a>
+                </li>
+            </ul>
+            <a class="btn navbar-btn ml-2 text-white btn-secondary">
+                <i class="fa d-inline fa-lg fa-user-circle-o"></i> Sign in</a>
+        </div>
+    </div>
+</nav>
+
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    </ol>
+    <div class="carousel-inner" role="listbox">
+        <div class="carousel-item active">
+            <img class="img-fluid" src="assets/images/carousel1.jpg" alt="First slide">
+            <div class="carousel-caption">
+                <h3>Los Angeles</h3>
+                <p>LA is always so much fun!</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="img-fluid" src="assets/images/carousel2.jpg" alt="Second slide">
+            <div class="carousel-caption">
+                <h3>Los Angeles</h3>
+                <p>LA is always so much fun!</p>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <img class="img-fluid" src="assets/images/carousel3.jpg" alt="Third slide">
+            <div class="carousel-caption">
+                <h3>Los Angeles</h3>
+                <p>LA is always so much fun!</p>
+            </div>
+        </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</div>
+
+<!--Section Feature-->
+
+<div class="py-5 bg-light" >
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-md-12">
+                <h1 class="mb-4 text-primary">Benefits and features</h1>
+                <p class="lead">Design unique interfaces by customizing Bootstrap theme with Pingendo.
+                    <br>Modify colors, fonts and grid setting without opening any CSS file.</p>
+                <div class="row text-left mt-5">
+                    <div class="col-md-4 my-3">
+                        <div class="row mb-3">
+                            <div class="text-center col-2">
+                                <i class="d-block mx-auto fa fa-3x fa-calendar-o"></i>
+                            </div>
+                            <div class="align-self-center col-10">
+                                <h5 class="text-secondary">
+                                    <b>For him</b>
+                                </h5>
+                            </div>
+                        </div>
+                        <p>Based on fluid design principles. Works with screen resolution. &nbsp;In-app chat panel 24/7 active. The support you need, right there.</p>
+                    </div>
+                    <div class="col-md-4 my-3">
+                        <div class="row mb-3">
+                            <div class="text-center col-2">
+                                <i class="d-block mx-auto fa fa-3x fa-pencil-square-o"></i>
+                            </div>
+                            <div class="align-self-center col-10">
+                                <h5 class="text-secondary">
+                                    <b>For her</b>
+                                </h5>
+                            </div>
+                        </div>
+                        <p>Help us spreading the word. Tell your friends with just one-click.&nbsp;Work simultaneously on different panels with the switcher</p>
+                    </div>
+                    <div class="col-md-4 my-3">
+                        <div class="row mb-3">
+                            <div class="text-center col-2">
+                                <i class="d-block mx-auto fa fa-3x fa-bullhorn"></i>
+                            </div>
+                            <div class="align-self-center col-10">
+                                <h5 class="text-secondary">
+                                    <b>For its</b>
+                                </h5>
+                            </div>
+                        </div>
+                        <p>You are working with plain HTML and SASS files on your computer, so extending and integrating Javascript is a breeze.</p>
+                    </div>
+                    <div class="col-md-4 my-3">
+                        <div class="row mb-3">
+                            <div class="text-center col-2">
+                                <i class="d-block mx-auto fa fa-3x fa-book"></i>
+                            </div>
+                            <div class="align-self-center col-10">
+                                <h5 class="text-secondary">
+                                    <b>For anyone</b>
+                                </h5>
+                            </div>
+                        </div>
+                        <p>Based on fluid design principles. Works with screen resolution.&nbsp;Choose settings depending on the criteria you value the most.</p>
+                    </div>
+                    <div class="col-md-4 my-3">
+                        <div class="row mb-3">
+                            <div class="text-center col-2">
+                                <i class="d-block mx-auto fa fa-3x fa-book"></i>
+                            </div>
+                            <div class="align-self-center col-10">
+                                <h5 class="text-secondary">
+                                    <b>Even couples</b>
+                                </h5>
+                            </div>
+                        </div>
+                        <p>In-app chat panel 24/7 active. The support you need, right there. Pingendo enhance the battery duration and the quality of your life.&nbsp;</p>
+                    </div>
+                    <div class="col-md-4 my-3">
+                        <div class="row mb-3">
+                            <div class="text-center col-2">
+                                <i class="d-block mx-auto fa fa-3x fa-book"></i>
+                            </div>
+                            <div class="align-self-center col-10">
+                                <h5 class="text-secondary">
+                                    <b>Any other?</b>
+                                </h5>
+                            </div>
+                        </div>
+                        <p>Help us spreading the word. Tell your friends with just one-click.&nbsp;Work simultaneously on different panels. Share the work with teammates.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--Footer Section-->
+<div class="py-4 bg-dark text-white" >
+    <div class="container">
+        <div class="row">
+            <div class="p-4 col-md-3">
+                <h2 class="mb-4">ติดต่อเรา</h2>
+                <p>
+                    <a href="tel:+246 - 542 550 5462" class="text-white">
+                        <i class="fa d-inline mr-3 text-secondary fa-phone"></i>+246 - 542 550 5462</a>
+                </p>
+                <p>
+                    <a href="mailto:info@pingendo.com" class="text-white">
+                        <i class="fa d-inline mr-3 text-secondary fa-envelope-o"></i>info@pingendo.com</a>
+                </p>
+                <p>
+                    <a href="https://goo.gl/maps/AUq7b9W7yYJ2" class="text-white" target="_blank">
+                        <i class="fa d-inline mr-3 fa-map-marker text-secondary"></i>365 Park Street, NY</a>
+                </p>
+            </div>
+            <div class="p-4 col-md-3">
+                <h2 class="mb-4 text-light">Subscribe</h2>
+                <form>
+                    <fieldset class="form-group text-white">
+                        <label for="exampleInputEmail1">Get our newsletter</label>
+                        <input type="email" class="form-control" placeholder="Enter email"> </fieldset>
+                    <button type="submit" class="btn btn-outline-secondary">Submit</button>
+                </form>
+            </div>
+            <div class="p-4 col-md-3">
+                <h2 class="mb-4 text-secondary">Pingendo</h2>
+                <p class="text-white">A company for whatever you may need, from website prototyping to publishing</p>
+            </div>
+            <div class="p-4 col-md-3">
+                <h2 class="mb-4 text-secondary">Mapsite</h2>
+                <ul class="list-unstyled">
+                    <a href="#" class="text-white">Home</a>
+                    <br>
+                    <a href="#" class="text-white">About us</a>
+                    <br>
+                    <a href="#" class="text-white">Our services</a>
+                    <br>
+                    <a href="#" class="text-white">Stories</a>
+                </ul>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-md-12 mt-3">
+                <p class="text-center text-white">© Copyright 2017 TACE - All rights reserved. </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</body>
+</html>
